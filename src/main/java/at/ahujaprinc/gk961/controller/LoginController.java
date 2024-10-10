c67db001dbec843b48df37af7c00be48d5efeb29
Ankush <aahuja@student.tgm.ac.at>
2024-10-10 15:41:19 +0200

    Create base classes for all controllers

--- src/main/java/at/ahujaprinc/gk961/controller/LoginController.java
+++ src/main/java/at/ahujaprinc/gk961/controller/LoginController.java
@@ -6,17 +6,17 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import at.ahujaprinc.gk961.model.RegistryRequest;
-import at.ahujaprinc.gk961.model.RegistryResponse;
+import at.ahujaprinc.gk961.model.LoginRequest;
+import at.ahujaprinc.gk961.model.LoginResponse;
 
 /**
  * RegistrationController
  */
 @RestController
-@RequestMapping("/auth/admin/register")
-public class RegistrationController {
+@RequestMapping("/auth/signin")
+public class LoginController {
   @PostMapping
-  public RegistryResponse register(@RequestBody RegistryRequest request) {
+  public LoginResponse signin(@RequestBody LoginRequest request) {
     return null;
   }
 }
---
+++ src/main/java/at/ahujaprinc/gk961/controller/VerifyController.java
@@ -0,0 +1,21 @@
+package at.ahujaprinc.gk961.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import at.ahujaprinc.gk961.model.VerifyRequest;
+import at.ahujaprinc.gk961.model.VerifyResponse;
+
+/**
+ * RegistrationController
+ */
+@RestController
+@RequestMapping("/auth/verify")
+public class VerifyController {
+  @GetMapping
+  public VerifyResponse verify(@RequestBody VerifyRequest request) {
+    return null;
+  }
+}
