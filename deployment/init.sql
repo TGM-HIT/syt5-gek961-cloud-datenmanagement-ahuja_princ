71a497df45c58c9e8527661d03c8c6d715f7315c
Ankush <aahuja@student.tgm.ac.at>
2024-10-10 17:08:19 +0200

    Add DB connection

--- deployment/init.sql
+++ deployment/init.sql
@@ -2,11 +2,11 @@ CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
+    password INTEGER NOT NULL,
     roles TEXT[]
 );
 
 -- Beispiel-Eintrag für einen Benutzer mit mehreren Rollen
-INSERT INTO users (name, email, password, roles)
+INSERT INTO users (name, username, password, roles)
 VALUES
-    ('John Doe', 'johndoe@example.com', 'encrypted_password_here', ARRAY['ADMIN', 'READER']);
+    ('John Doe', 'johndoe@example.com', 69490486, ARRAY['ADMIN', 'READER']); -- 69490486 = Hallo
